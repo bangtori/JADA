f240ae319bffabb556ab96ae2e9cb0d8347a58cd
Tori <ttang1135@naver.com>
2024-01-08 00:57:20 +0900

    feat: 기본 Constraints 추가 (Screen)

---
+++ JADA/JADA/Constraints/Screen.swift
@@ -0,0 +1,13 @@
+//
+//  Screen.swift
+//  JADA
+//
+//  Created by 방유빈 on 2024/01/08.
+//
+
+import UIKit
+
+struct Screen {
+    static let height = UIScreen.main.bounds.height
+    static let width = UIScreen.main.bounds.width
+}
