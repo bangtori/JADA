0e34a5e507e00be43534a7c4943776911c851bb3
Tori <ttang1135@naver.com>
2024-01-14 21:47:40 +0900

    feat: API Service 추가

---
+++ JADA/JADA/Model/SentimentModel.swift
@@ -0,0 +1,8 @@
+//
+//  SentimentModel.swift
+//  JADA
+//
+//  Created by 방유빈 on 2024/01/14.
+//
+
+import Foundation
---
+++ JADA/JADA/Service/ClovaApiService.swift
@@ -0,0 +1,8 @@
+//
+//  ClovaApiService.swift
+//  JADA
+//
+//  Created by 방유빈 on 2024/01/13.
+//
+
+import Foundation
